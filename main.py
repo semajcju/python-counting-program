7d9b831af459d13b78ba6f7d4d92d3ed3bb18164
semajcju <007lambthegrat@gmail.com>
2021-08-31 18:02:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import pyautogui as auto
 import time
 
-num = str(160001)
+num = str(160128)
 
 time.sleep(5)
 
-while True:
+while True:134
+1601
     num = str(num)
     auto.typewrite(num)
     num = int(num)
     num = num + 1
     auto.press('enter')
-    time.sleep(1)
-    if num == 170001160132
-    160133
-    :
+    time.sleep(1.6160)
+    if num == 170001:
         break
