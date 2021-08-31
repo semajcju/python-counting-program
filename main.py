a7c1b67fa41f7a60a12e4518808d29905d0f2001
semajcju <007lambthegrat@gmail.com>
2021-08-31 18:40:52 +0100

    Update main.py
    optimised while loop and setup

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pyautogui as auto
 import time
 
+##setup the numbers
 
 num = input('please input number you want to start counting from: ')
-endnum = input('please input the number you would like to stop counting from: ')
+endnum = int(input('please input the number you would like to stop counting from: ')) + 1
+
+##this is where you tab out
 
 print('beginning count in ')
 print('5')
@@ -17,9 +20,12 @@ time.sleep(1)
 print('1')
 time.sleep(1)
 
+##simple while loop to type every number
 
-while num != endnum:
+while int(num) != endnum:
     auto.typewrite(str(num))
     num = int(num) + 1
     auto.press('enter')
+    ##this amount of time stops discord from thinking you are spamming
+    ##feel free to change it
     time.sleep(1.7)
